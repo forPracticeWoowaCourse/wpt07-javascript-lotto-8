bd59262bbecddc43ca15c9ff0712f1a2832e654b
SeongUk52 <rkfka7849@gmail.com>
2026-01-09 17:17:47 +0900

    feat: 당첨번호와 보너스 번호를 받아 등수 반환

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -12,7 +12,25 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  calculateRanking(numbers, bonus) {
+    const {count, isBonus} = this.#countSameAndBonus(numbers, bonus);
+    if(count === 6) return 1;
+    if(count === 5 && isBonus) return 2;
+    if(count === 5) return 3;
+    if(count === 4) return 4;
+    if(count === 3) return 5;
+    return false;
+  }
+
+  #countSameAndBonus(numbers, bonus) {
+    let count = 0;
+    let isBonus = false;
+    this.#numbers.forEach(v => {
+      if(numbers.includes(v)) count++;
+      if(bonus === v) isBonus = true;
+    })
+    return {count: count, isBonus: isBonus};
+  }
 }
 
 export default Lotto;
